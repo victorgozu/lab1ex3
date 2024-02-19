937ec18a5ca3821fcc5c687eece89ccaad534ea5
victor.gozu <victor.gozu@MP150159>
2024-02-19 18:33:46 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * Ex3
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura
+ (google: how to get the last digit of a number)
+*/
+Console.WriteLine("Introduceti un numar intreg din mai multe cifre: ");
+int numarTastatura = int.Parse(Console.ReadLine());
+
+int lastDigit = numarTastatura % 10;
+
+Console.WriteLine("Ultima cifra a numarului introdus este: " + lastDigit);
+
